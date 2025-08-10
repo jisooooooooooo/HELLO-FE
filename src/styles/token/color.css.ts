814d836407b8fd75b328a85932586ca86c29d433
jisooooooooooo <dlawltn0708@naver.com>
2025-08-10 07:18:51 +0000

    🚀 Latest deployment

--- src/styles/token/color.css.ts
+++ src/styles/token/color.css.ts
@@ -1,4 +1,32 @@
 export const colors = {
-  black: '#000000',
-  white: '#FFFFFF',
+  black01: '#121212',
+  black01_70: 'rgba(18, 18, 18, 0.7)',
+
+  white01: '#FFFFFF',
+
+  grey01: '#17191C',
+  grey02: '#1E2127',
+  grey03: '#282C33',
+  grey04: '#2E323A',
+  grey05: '#41454C',
+  grey06: '#5A5E66',
+  grey07: '#999DA4',
+  grey08: '#B1B6BE',
+  grey09: '#C5C8CE',
+
+  blue01: '#C3D9FF',
+  blue02: '#8AB4FF',
+  blue03: '#74A6FF',
+  blue04: '#74A6FF',
+  blue05: '#3E72F3',
+  blue06: '#2F53C4',
+  blue07: '#1B49D7',
+  blue08: '#2C4A83',
+
+  yellow01: '#FFFEDE',
+  yellow02: '#FFF9A9',
+  yellow03: '#FFF677',
+  yellow04: '#FFCE3B',
+
+  error01: '#FF0000',
 };
